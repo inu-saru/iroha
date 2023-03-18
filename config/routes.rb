0c30686b39d68f0f3a9de311724d813f73b896b0
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-03-18 18:05:45 +0900

    6e8e9b87 fix: Dont use name space for devise on routes, it makes bug on devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'api/v1',
+                     path_names: {
+                       sign_in: 'login',
+                       sign_out: 'logout',
+                       registration: 'signup'
+                     },
+                     controllers: {
+                       sessions: 'api/v1/users/sessions',
+                       registrations: 'api/v1/users/registrations'
+                     }
   namespace :api do
     namespace :v1 do
       resources :ping, only: :index
-      devise_for :users, path: '',
-                         path_names: {
-                           sign_in: 'login',
-                           sign_out: 'logout',
-                           registration: 'signup'
-                         },
-                         controllers: {
-                           sessions: 'api/v1/users/sessions',
-                           registrations: 'api/v1/users/registrations'
-                         }
     end
   end
 end
