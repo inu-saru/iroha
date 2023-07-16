67cf232aafbf50c757559810d62a6a9b46de6887
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-07-16 12:22:13 +0900

    e741c464 add: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+3.times do |i|
+  user = FactoryBot.create(:user, email: "test#{i}@test.com", password: ENV.fetch("SEED_USER_PASSWORD", nil))
+  space_user = FactoryBot.create(:a_space_the_user, user:)
+  # sectionなしのsentence
+  3.times { FactoryBot.create(:sentence, space: space_user.space) }
+  # sectionありのsentence
+  3.times do
+    section = FactoryBot.create(:section, space: space_user.space)
+    3.times { FactoryBot.create(:sentence, space: space_user.space, section:) }
+  end
+end
