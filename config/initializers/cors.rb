c54d1da6e2bd71ac5927ceba2149dd92161907a4
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-08-05 13:02:27 +0900

    709577d7 update: expose headers key that associate pagenation

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource(
       '*',
       headers: :any,
-      expose: %w[access-token expiry token-type Authorization],
+      expose: %w[access-token expiry token-type Authorization Current-Page Link Total-Count Total-Pages],
       methods: [:get, :patch, :put, :delete, :post, :options, :show]
     )
   end
