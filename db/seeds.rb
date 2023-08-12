6f70052c4b1f7d2160a41936be3604233ab40a0d
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-08-12 22:19:22 +0900

    e741c464 update: use csv file on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
+require "csv"
+
 3.times do |i|
   user = FactoryBot.create(:user, email: "test#{i}@test.com", password: ENV.fetch("SEED_USER_PASSWORD", nil))
   space_user = FactoryBot.create(:a_space_the_user, user:)
   # sectionなしのsentence
   3.times { FactoryBot.create(:sentence, space: space_user.space) }
   # sectionありのsentence
-  3.times do
-    section = FactoryBot.create(:section, space: space_user.space)
-    3.times { FactoryBot.create(:sentence, space: space_user.space, section:) }
+  CSV.foreach('db/csv/duo_light.csv') do |row|
+    section = Section.find_or_create_by(space: space_user.space, name: row[0])
+    FactoryBot.create(:sentence, space: space_user.space, en: row[1], ja: row[2], section:)
   end
 end
