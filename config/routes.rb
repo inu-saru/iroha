c24b93fe59e23fd0dee7d1e195f57d47c1cf965b
inu_saru <inu_saru_eagle@yahoo.co.jp>
2023-10-10 19:23:29 +0900

    a8b2c11a update: add comment about batch routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
                      }
   namespace :api do
     namespace :v1 do
+      # 'api/v1/batch' はmiddleware管理 -> lib/middlewares/batch_api.rb
       resources :ping, only: :index
       namespace :users do
         get 'me', to: 'me#show'
