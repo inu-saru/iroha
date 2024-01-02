600e396ae73daaac468ef13251f95a2e030a6e37
inu_saru <inu_saru_eagle@yahoo.co.jp>
2024-01-02 10:56:21 +0700

    864c5a46 WIP update: adjust seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 require "csv"
 
-3.times do |i|
+1.times do |i|
   user = FactoryBot.create(:user, email: "test#{i}@test.com", password: ENV.fetch("SEED_USER_PASSWORD", nil))
   space_user = FactoryBot.create(:a_space_the_user, user:)
   space = space_user.space
   # sectionなしのsentence
-  3.times { FactoryBot.create(:sentence, space:) }
+  # 3.times { FactoryBot.create(:sentence, space:) }
   # sectionありのsentence
-  CSV.foreach('db/csv/duo_light.csv', headers: true) do |row|
+  CSV.foreach('db/csv/duo.csv', headers: true) do |row|
     section = Section.find_or_create_by(space:, name: row['section'])
     FactoryBot.create(:sentence, space:, en: row['en'], ja: row['ja'], section:)
   end
   # relationship
-  CSV.foreach('db/csv/duo_word_light.csv', headers: true) do |row|
+  CSV.foreach('db/csv/duo_word.csv', headers: true) do |row|
     sentence = Vocabulary.find_by(space:, en: row['followed'])
     word = Vocabulary.create(space:, section: sentence.section, en: row['en'], ja: row['ja'])
     Relationship.create(space:, followed: sentence, follower: word, positions: row['positions'])
