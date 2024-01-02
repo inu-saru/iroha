d65416d197ff984b65f536891dbe18d4dd9798b6
inu_saru <inu_saru_eagle@yahoo.co.jp>
2024-01-02 12:22:54 +0700

    864c5a46 WIP update: adjust seed to light

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@ require "csv"
   # sectionなしのsentence
   # 3.times { FactoryBot.create(:sentence, space:) }
   # sectionありのsentence
-  CSV.foreach('db/csv/duo.csv', headers: true) do |row|
+  CSV.foreach('db/csv/duo_light.csv', headers: true) do |row|
     section = Section.find_or_create_by(space:, name: row['section'])
     FactoryBot.create(:sentence, space:, en: row['en'], ja: row['ja'], section:)
   end
   # relationship
-  CSV.foreach('db/csv/duo_word.csv', headers: true) do |row|
+  CSV.foreach('db/csv/duo_word_light.csv', headers: true) do |row|
     sentence = Vocabulary.find_by(space:, en: row['followed'])
     word = Vocabulary.create(space:, section: sentence.section, en: row['en'], ja: row['ja'])
     Relationship.create(space:, followed: sentence, follower: word, positions: row['positions'])
