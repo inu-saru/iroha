a89aa83fdc98952cbfa2d27b93673409ae1e243f
inu_saru <inu_saru_eagle@yahoo.co.jp>
2024-06-30 14:33:27 +0700

    e598b872 fix: seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,18 @@ require "csv"
   # sectionなしのsentence
   # 3.times { FactoryBot.create(:sentence, space:) }
   # sectionありのsentence
-  CSV.foreach('db/csv/duo_light.csv', headers: true) do |row|
-    section = Section.find_or_create_by(space:, name: row['section'])
-    FactoryBot.create(:sentence, space:, en: row['en'], ja: row['ja'], section:)
+  CSV.foreach('db/csv/duo.csv', headers: true).each_slice(100) do |rows|
+    rows.each do |row|
+      section = Section.find_or_create_by(space:, name: row['section'])
+      FactoryBot.create(:sentence, space:, en: row['en'], ja: row['ja'], section:)
+    end
   end
   # relationship
-  CSV.foreach('db/csv/duo_word_light.csv', headers: true) do |row|
-    sentence = Vocabulary.find_by(space:, en: row['followed'])
-    word = Vocabulary.create(space:, section: sentence.section, en: row['en'], ja: row['ja'])
-    Relationship.create(space:, followed: sentence, follower: word, positions: row['positions'])
+  CSV.foreach('db/csv/duo_word.csv', headers: true).each_slice(100) do |rows|
+    rows.each do |row|
+      sentence = Vocabulary.find_by(space:, en: row['followed'])
+      word = Vocabulary.create(space:, section: sentence.section, en: row['en'], ja: row['ja'])
+      Relationship.create(space:, followed: sentence, follower: word, positions: row['positions'])
+    end
   end
 end
